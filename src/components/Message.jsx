73ea34f0d12fc4c996baca25ac9542bab7caa8fa
Tiaguh <tiagosingup@gmail.com>
2024-06-18 11:22:17 -0300

    improving message component

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
-function Message() {
+export default function Message() {
   return (
-    <div>Message</div>
+    <div className="message owner">
+      <div className="messageInfo">
+        <img src="https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+        <span>just now</span>
+      </div>
+
+      <div className="messageContent">
+        <p>Hello</p>
+        {/* <img src="https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" /> */}
+      </div>
+
+    </div>
   )
 }
-
-export default Message
