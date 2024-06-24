afe78021f196d5ea30e2a5b819a3dc8a161cbb6f
Tiaguh <tiagosingup@gmail.com>
2024-06-24 08:00:01 -0300

    first steps in image upload

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../firebase";
+import React, { useState } from 'react';
+
+import { auth, storage } from "../firebase";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+
 import { MdPhoto } from "react-icons/md";
 
 export default function Register() {
-    const handleSubmit = (e) => {
+    const [error, setError] = useState(false)
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
         const displayName = e.target[0].value;
@@ -12,16 +17,29 @@ export default function Register() {
         const password = e.target[2].value;
         const file = e.target[3].files[0];
 
-        createUserWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                const user = userCredential.user;
-                console.log(user);
-            })
-            .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                console.error(`Error (${errorCode}): ${errorMessage}`);
-            });
+        try {
+            const response = await createUserWithEmailAndPassword(auth, email, password)
+
+            const storageRef = ref(storage, displayName);
+
+            const uploadTask = uploadBytesResumable(storageRef, file);
+
+            uploadTask.on(
+                (error) => {
+                    setError(true)
+                },
+                () => {
+                    getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+                        await updateProfile(response.user, {
+                            displayName,
+                            photoURL: downloadURL,
+                        })
+                    });
+                }
+            );
+        } catch (error) {
+            setError(true)
+        }
     }
 
     return (
@@ -43,6 +61,7 @@ export default function Register() {
                         <h3>Add an avatar</h3>
                     </label>
                     <button>Sign Up</button>
+                    {error && <span>Something went wrong</span>}
                 </form>
                 <p>Already have an account? Login</p>
             </div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCl4SU6nZ_EhWwVxmhyXa1EMRwnWvIkdXQ",
@@ -12,3 +13,4 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const storage = getStorage();
