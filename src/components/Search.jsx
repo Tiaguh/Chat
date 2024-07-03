bcfde68bc5a4987fd6e498f35e3a284dfc7823b6
Tiaguh <tiagosingup@gmail.com>
2024-07-02 22:02:34 -0300

    first steps in chat

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,16 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 
-import { collection, query, where, getDocs } from "firebase/firestore";
+import { collection, query, where, getDocs, doc, setDoc } from "firebase/firestore";
 import { db } from "../firebase"
+
+import { AuthContext } from "../context/AuthContext"
 
 function Search() {
   const [userName, setUserName] = useState("")
   const [user, setUser] = useState(null)
   const [err, setErr] = useState(null)
+
+  const { currentUser } = useContext(AuthContext)
 
   const handleSearch = async () => {
     const q = query(collection(db, "users"), where("displayName", "==", userName))
@@ -25,6 +29,21 @@ function Search() {
     e.code === "Enter" && handleSearch();
   }
 
+  const handleSelect = async () => {
+    const combinedId = currentUser.uid > user.uid ? currentUser.uid + user.uid : user.uid + currentUser.uid
+
+    try {
+      const res = await getDocs(db, "users", combinedId)
+
+      if (!res.exists()) {
+        await setDoc(doc, (db,"chats", combinedId), { messages: [] })
+      }
+    } catch (err) {
+
+    }
+
+  }
+
   return (
     <div className="search">
       <div className="searchForm">
@@ -36,7 +55,7 @@ function Search() {
         />
       </div>
       {err && <span>User not found!</span>}
-      {user && <div className="userChat">
+      {user && <div className="userChat" onClick={handleSelect} >
         <img src={user.photoUrl} />
         <div className="userChatInfo">
           <span>{user.displayName}</span>
