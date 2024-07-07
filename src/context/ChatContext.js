30926a57a0b3291a4d622d090758659fc4cfee04
Tiaguh <tiagosingup@gmail.com>
2024-07-07 20:49:46 -0300

    creating the chat reducer

--- src/context/ChatContext.js
+++ src/context/ChatContext.js
@@ -1,9 +1,6 @@
-import {
-    createContext,
-    useContext,
-    useReducer,
-} from "react";
-
+import { createContext, useContext, useReducer } from "react";
+import { auth } from "../firebase";
+import { onAuthStateChanged } from "firebase/auth";
 import { AuthContext } from "./AuthContext";
 
 export const ChatContext = createContext();
@@ -15,8 +12,26 @@ export const ChatContextProvider = ({ children }) => {
         user: {},
     };
 
+    const chatReducer = (state, action) => {
+        switch (action.type) {
+            case "CHANGE_USER":
+                return {
+                    user: action.payload,
+                    chatId:
+                        currentUser.uid > action.payload.uid
+                            ? currentUser.uid + action.payload.uid
+                            : action.payload.uid + currentUser.uid,
+                };
+
+                default: 
+                    return state;
+        }
+    };
+
+    const [state, dispatch] = useReducer(chatReducer, INITIAL_STATE);
+
     return (
-        <ChatContext.Provider value={{ currentUser }}>
+        <ChatContext.Provider value={{ data:state, dispatch }}>
             {children}
         </ChatContext.Provider>
     );
