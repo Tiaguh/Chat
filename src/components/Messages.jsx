263566ecfc9c5b483ec80dcf9c51c7221f8ad219
Tiaguh <tiagosingup@gmail.com>
2024-07-09 22:28:32 -0300

    making messages component

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,69 +1,32 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import Message from "./Message"
 
+import { ChatContext } from '../context/ChatContext';
+import { doc, onSnapshot } from 'firebase/firestore';
+import { db } from '../firebase';
+
 export default function Messages() {
+  const [messages, setMessages] = ([])
+  const { data } = useContext(ChatContext)
+
+  useEffect(() => {
+    const unSub = onSnapshot(doc(db, "chats", data.chatId), (doc) => {
+      doc.exists() && setMessages(doc.data().messages)
+    })
+
+    return () => {
+      unSub()
+    }
+  }, [data.chatId])
+
   return (
     <div className="messages">
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-
+      {messages.map(m => (
+        <Message
+          message={m}
+          key={m.id}
+        />
+      ))}
     </div>
   )
 }
