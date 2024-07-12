c70a212f92ba4688901d7f3d07a425610d7e010d
Tiaguh <tiagosingup@gmail.com>
2024-07-11 22:16:36 -0300

    creating chat upload img

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -5,6 +5,11 @@ import { TfiClip } from "react-icons/tfi";
 
 import { AuthContext } from '../context/AuthContext';
 import { ChatContext } from '../context/ChatContext';
+import { Timestamp, arrayUnion, doc, updateDoc } from 'firebase/firestore';
+
+import { v4 as uuid } from "uuid"
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
+import { storage } from '../firebase';
 
 export default function Input() {
   const [text, setText] = useState("")
@@ -13,8 +18,46 @@ export default function Input() {
   const { currentUser } = useContext(AuthContext)
   const { data } = useContext(ChatContext)
 
-  const handleSend = () => {
+  const handleSend = async () => {
+    if (img) {
+      const storageRef = ref(storage, uuid);
 
+      const uploadTask = uploadBytesResumable(storageRef, img);
+
+      uploadTask.on(
+        (error) => {
+          setError(true)
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+            await updateProfile(response.user, {
+              displayName,
+              photoURL: downloadURL,
+            })
+            await setDoc(doc(db, "users", response.user.uid), {
+              uid: response.user.uid,
+              displayName,
+              email,
+              photoURL: downloadURL,
+            })
+
+            await setDoc(doc(db, "userChats", response.user.uid), {})
+            navigate("/")
+          });
+        }
+      );
+
+
+    } else {
+      await updateDoc(doc(db, "chats", data.chatId), {
+        messages: arrayUnion({
+          id: uuid,
+          text,
+          senderId: currentUser.uid,
+          date: Timestamp.now()
+        })
+      })
+    }
   }
 
   return (
@@ -22,7 +65,7 @@ export default function Input() {
       <input
         type="text"
         placeholder="Type something..."
-        onChange={e=>setText(e.target.value)}
+        onChange={e => setText(e.target.value)}
       />
       <div className="send">
         <MdPhotoLibrary
@@ -35,7 +78,7 @@ export default function Input() {
           type="file"
           style={{ display: "none" }}
           id="file"
-          onChange={e=>setImg(e.target.files[0])}
+          onChange={e => setImg(e.target.files[0])}
         />
 
         <label htmlFor="file">
