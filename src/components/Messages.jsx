c3f12578e4561f4528c7c684dded936031ed6684
Tiaguh <tiagosingup@gmail.com>
2024-07-15 21:59:48 -0300

    fixing bugs

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,32 +1,34 @@
-import React, { useContext, useEffect } from 'react';
-import Message from "./Message"
+import React, { useContext, useEffect, useState } from "react";
+import Message from "./Message";
 
-import { ChatContext } from '../context/ChatContext';
-import { doc, onSnapshot } from 'firebase/firestore';
-import { db } from '../firebase';
+import { ChatContext } from "../context/ChatContext";
 
-export default function Messages() {
-  const [messages, setMessages] = ([])
-  const { data } = useContext(ChatContext)
+import { doc, onSnapshot } from "firebase/firestore";
+import { db } from "../firebase";
+
+const Messages = () => {
+  const [messages, setMessages] = useState([]);
+  const { data } = useContext(ChatContext);
 
   useEffect(() => {
     const unSub = onSnapshot(doc(db, "chats", data.chatId), (doc) => {
-      doc.exists() && setMessages(doc.data().messages)
-    })
+      doc.exists() && setMessages(doc.data().messages);
+    });
 
     return () => {
-      unSub()
-    }
-  }, [data.chatId])
+      unSub();
+    };
+  }, [data.chatId]);
+
+  console.log(messages)
 
   return (
     <div className="messages">
-      {messages.map(m => (
-        <Message
-          message={m}
-          key={m.id}
-        />
+      {messages.map((m) => (
+        <Message message={m} key={m.id} />
       ))}
     </div>
-  )
-}
+  );
+};
+
+export default Messages;
